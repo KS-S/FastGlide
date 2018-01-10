c1930c55730ab34c80804c33efcebbf90e121dbb
Steven.wang <Steven.wang@firstleap.cn>
2018-01-10 18:01:50 +0800

    add library

--- app/src/main/java/cn/ks/fastglide/MainActivity.java
+++ app/src/main/java/cn/ks/fastglide/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import cn.ks.mylibrary.TestUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity: ", "Android Studio上传代码到GitHub");
+//        Log.d("MainActivity: ", "Android Studio上传代码到GitHub");
+        TestUtils.toast(this, "Android Studio制作自己的依赖库");
     }
 }
